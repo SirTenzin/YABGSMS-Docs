2a582de5bd9cc9d4f7a14fa0a31ca514a9dda5d9
SirTenzin <49116958+SirTenzin@users.noreply.github.com>
2025-03-02 00:19:50 +0000

    Add Open Next.js configuration for Cloudflare and update project settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,13 @@
 import { createMDX } from 'fumadocs-mdx/next';
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+ 
+initOpenNextCloudflareForDev();
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  // This is important for Cloudflare Pages
-  output: 'export',
-  // If you're using images, you'll need to configure them for static export
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default withMDX(config);
